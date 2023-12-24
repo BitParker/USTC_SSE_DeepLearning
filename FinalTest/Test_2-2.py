ab73dbdfefbd19f0784c9a8fec9d2a1d76d4d120
Parker <pengkai_xie@mail.ustc.edu.cn>
2023-12-24 12:35:15 +0800

    Add files via upload

---
+++ FinalTest/Test_2-2.py
@@ -0,0 +1,90 @@
+# test2 - 方案2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+import time
+
+# GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 数据加载
+train_data = datasets.MNIST(root='./', train=True, transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.1307, ], [0.3081, ])
+]), download=True)
+
+train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
+
+test_data = datasets.MNIST(root='./', train=False, transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.1307, ], [0.3081, ])
+]))
+test_loader = DataLoader(dataset=test_data, batch_size=64, shuffle=True)
+
+# 网络
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1_192 = nn.Conv2d(1, 4, 3, padding=1)  # 输入1个通道，输出4个通道，3*3卷积，填充1
+        self.bn1_192 = nn.BatchNorm2d(4)
+        self.conv2_192 = nn.Conv2d(4, 8, 3, padding=1)
+        self.bn2_192 = nn.BatchNorm2d(8)
+        self.conv3_192 = nn.Conv2d(8, 8, 3, padding=1)
+        self.bn3_192 = nn.BatchNorm2d(8)
+        self.pool_192 = nn.MaxPool2d(4, 4)
+        self.fc1_192 = nn.Linear((28 * 28) // (4 * 4) * 8, 512)
+        self.fc2_192 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = F.relu(self.bn1_192(self.conv1_192(x)))
+        x = F.relu(self.bn2_192(self.conv2_192(x)))
+        x = F.relu(self.bn3_192(self.conv3_192(x)))
+        x = self.pool_192(x)
+        x = x.view(-1, (28 * 28) // (4 * 4) * 8)
+        x = self.fc1_192(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.fc2_192(x)
+        return x
+
+# 初始化模型
+net = Model().to(device)
+loss_fn = nn.CrossEntropyLoss()
+opt = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)  
+
+# 训练
+start_time = time.time()
+for epoch in range(20):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader):
+        x, y = data
+        x, y = x.to(device), y.to(device)
+        net.train()
+        opt.zero_grad()
+        pred = net(x)
+        loss = loss_fn(pred, y)
+        loss.backward()
+        opt.step()
+        running_loss += loss.item()
+    if epoch % 5 == 0:
+        print('Epoch %d, Loss: %.3f' % (epoch, running_loss / len(train_loader)))
+end_time = time.time()
+
+training_time = end_time - start_time
+print(f"Time: {training_time} seconds")
+
+# 测试
+rights = 0
+length = 0
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        x, y = data
+        x, y = x.to(device), y.to(device)
+        net.eval()
+        pred = net(x)
+        _, pred_labels = torch.max(pred, 1)
+        rights += torch.sum(pred_labels == y).item()
+        length += len(y)
+print (rights, length, rights/ length)
